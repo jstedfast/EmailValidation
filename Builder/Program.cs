9899e2d75d8011aba25b62903b1451ed986451d1
Berta Balázs <95berta.balazs@gmail.com>
2017-04-15 15:29:20 +0200

    Simple Email tester cmd app.

---
+++ Builder/Program.cs
@@ -0,0 +1,30 @@
+﻿using EmailValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            do
+            {
+                Console.Write("Enter an email address: ");
+
+                var input = Console.ReadLine();
+                if (input == null)
+                    break;
+
+                input = input.Trim();
+                Console.WriteLine("{0} is {1}!", input, EmailValidator.Validate(input) ? "valid" : "invalid");
+            } while (true);
+
+            Console.WriteLine();
+        }
+    }
+}
