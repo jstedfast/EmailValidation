a0310875cec8fe087e6033198d337f85182c4fd3
Jeffrey Stedfast <jestedfa@microsoft.com>
2022-02-23 10:11:20 -0500

    Bumped version to 1.0.7
    * Dropped support for .NET Framework v4.5.
    * Added support for .NET Framework v4.5.2.
    * Dropped support for .NETStandard v1.1.
    * Added support for .NET 6.0.

--- UnitTests/EmailValidationTests.cs
+++ UnitTests/EmailValidationTests.cs
@@ -3,7 +3,7 @@
 //
 // Author: Jeffrey Stedfast <jestedfa@microsoft.com>
 //
-// Copyright (c) 2013-2021 Jeffrey Stedfast
+// Copyright (c) 2013-2022 Jeffrey Stedfast
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -252,7 +252,7 @@ namespace UnitTests
 			Assert.IsFalse (AreAttributesValid (target), "Email can't be null {0}", "");
 		}
 
-		bool AreAttributesValid (object target)
+		static bool AreAttributesValid (object target)
 		{
 			var context = new ValidationContext (target, null, null);
 			var results = new List<ValidationResult> ();
